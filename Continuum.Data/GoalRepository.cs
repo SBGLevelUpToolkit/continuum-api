b4fc201f52aab251fe4267e9e7c1c5515aa7ff06
nickmza <nickm40@gmail.com>
2015-09-03 14:41:56 +0200

    Add Missing Files

---
+++ Continuum.Data/GoalRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continuum.Data
+{
+    public class GoalRepository
+    {
+        private readonly Data.IContinuumDataContainer _container;
+
+        public GoalRepository(Data.IContinuumDataContainer container)
+        {
+            _container = container;
+        }
+
+        public IEnumerable<Data.Goal> GetActiveGoalsForTeam(Team team)
+        {
+            return _container.Goals.Where(i => i.TeamId == team.Id && i.Completed == false);
+        }
+
+        public void CreateGoal(Goal goal)
+        {
+            if (goal.DueDate < DateTime.Now)
+            {
+                throw new ApplicationException("Goals cannot have a Due Date in the past.");
+            }
+
+            _container.Goals.Add(goal);
+        }
+
+        public int SaveChanges()
+        {
+            return _container.SaveChanges();
+        }
+
+        public void UpdateGoal(Goal goal)
+        {
+            var goalToUpdate = _container.Goals.Find(goal.Id);
+            if (goalToUpdate == null)
+            {
+                throw new ApplicationException(string.Format("No goal with id {0}"));
+            }
+
+            goalToUpdate.CapabiltyId = goal.CapabiltyId;
+            goalToUpdate.Completed = goal.Completed;
+            goalToUpdate.Description = goal.Description;
+            goalToUpdate.DueDate = goal.DueDate;
+           
+        }
+
+        public void DeleteGoalById(int id)
+        {
+            var goalToDelete = _container.Goals.Find(id);
+            if (goalToDelete != null)
+            {
+                _container.Goals.Remove(goalToDelete);
+            }
+        }
+    }
+}
---
+++ Continuum.Data/Mocks/MockGoalsDBSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continuum.Data.Mocks
+{
+    public class MockGoalsDBSet : MockDbSet<Data.Goal>
+    {
+        public override Goal Find(params object[] keyValues)
+        {
+            return this.Where(i => i.Id == (int)keyValues.Single()).FirstOrDefault();
+        }
+    }
+}
