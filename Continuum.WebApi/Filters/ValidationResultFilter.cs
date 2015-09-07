5228b35e79662b3299da4e9c22b04f04df3f0fdd
nickmza <nickm40@gmail.com>
2015-09-07 14:38:08 +0200

    Add Validation Result Filter

---
+++ Continuum.WebApi/Filters/ValidationResultFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Continuum.WebApi.Filters
+{
+    public class ValidationResultFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception is DbEntityValidationException)
+            {
+                var exception = actionExecutedContext.Exception as DbEntityValidationException;
+
+                System.Text.StringBuilder builder = new System.Text.StringBuilder();
+
+                foreach(var validationIssue in exception.EntityValidationErrors)
+                {
+                    foreach (var propertyError in validationIssue.ValidationErrors)
+                    {
+                        builder.AppendFormat("{0} [{1}] {2}\r\n", validationIssue.Entry.Entity.GetType().Name, propertyError.PropertyName, propertyError.ErrorMessage);
+                    }   
+                }
+
+                actionExecutedContext.Response = ExceptionBuilder.CreateInternalServerError(builder.ToString(), "Validation").Response;
+            }
+        }
+    }
+}
