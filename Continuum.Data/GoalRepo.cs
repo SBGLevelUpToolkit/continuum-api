42dd129a91b60b0237c33de3b7a1b1bd0c175952
nickmza <nickm40@gmail.com>
2015-09-07 15:53:09 +0200

    Remove Load Code

--- Continuum.Data/GoalRepo.cs
+++ Continuum.Data/GoalRepo.cs
@@ -43,12 +43,13 @@ namespace Continuum.Data
 
         public void UpdateGoal(Goal goal)
         {
-            
+            /*
             var goalToUpdate = _container.Goals.Find(goal.Id);
             if (goalToUpdate == null)
             {
                 throw new ApplicationException(string.Format("No goal with id {0}"));
             }
+            */
 
             _container.Goals.Attach(goal);
             _container.SetStateForEntity(goal, System.Data.Entity.EntityState.Modified);
