867c49a57042dddc117052bb8f52a928388943e8
nickmza <nickm40@gmail.com>
2015-09-14 19:05:33 +0200

    Update Edit Code

--- Continuum.WebApi/Logic/GoalLogic.cs
+++ Continuum.WebApi/Logic/GoalLogic.cs
@@ -60,6 +60,21 @@ namespace Continuum.WebApi.Logic
 
         internal void UpdateGoalById(int id, Models.Goal goal)
         {
+               var team = _teamLogic.GetTeamForUser();
+
+            goal.Id = id;
+            _goalRepository.UpdateGoal(new Data.Goal()
+            {
+                CapabiltyId = goal.CapabilityId,
+                Description = goal.Notes,
+                Completed = goal.Completed,
+                Id = id,
+                DueDate = goal.DueDate,
+                TeamId = team.Id
+            });
+            _goalRepository.SaveChanges();
+
+            /*
             var team = _teamLogic.GetTeamForUser();
 
             var currentGoal = _goalRepository.GetGoalById(id);
@@ -79,7 +94,7 @@ namespace Continuum.WebApi.Logic
             else
             {
                 throw new ApplicationException("Invalid goal Id.");
-            }
+            }*/
         }
 
         internal void DeleteGoal(int id)
