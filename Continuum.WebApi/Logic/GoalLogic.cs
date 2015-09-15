9dd04fca3e1f06b6f5ccdd5b7e774dae24e673aa
nickmza <nickm40@gmail.com>
2015-09-15 15:16:18 +0200

    Update Edit Code...again

--- Continuum.WebApi/Logic/GoalLogic.cs
+++ Continuum.WebApi/Logic/GoalLogic.cs
@@ -61,24 +61,7 @@ namespace Continuum.WebApi.Logic
 
         internal void UpdateGoalById(int id, Models.Goal goal)
         {
-            var team = _teamLogic.GetTeamForUser();
-
-            goal.Id = id;
-            _goalRepository.UpdateGoal(new Data.Goal()
-            {
-                CapabiltyId = goal.CapabilityId,
-                Description = goal.Notes,
-                Completed = goal.Completed,
-                Id = id,
-                DueDate = goal.DueDate,
-                TeamId = team.Id,
-                Title = "Goal"
-            });
-            _goalRepository.SaveChanges();
-
-            /*
-            var team = _teamLogic.GetTeamForUser();
-
+          
             var currentGoal = _goalRepository.GetGoalById(id);
             if(currentGoal != null)
             {
@@ -86,17 +69,13 @@ namespace Continuum.WebApi.Logic
                 currentGoal.Description = goal.Notes;
                 currentGoal.CapabiltyId = goal.CapabilityId;
                 currentGoal.DueDate = goal.DueDate;
-
-                _goalRepository.UpdateGoal(currentGoal);
+               
                 _goalRepository.SaveChanges();
-
-                string message = String.Format("Updated Goal {0} Status: {1}", currentGoal.Id, currentGoal.Completed);
-                Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error() { Message = message });
             }
             else
             {
                 throw new ApplicationException("Invalid goal Id.");
-            }*/
+            }
         }
 
         internal void DeleteGoal(int id)
