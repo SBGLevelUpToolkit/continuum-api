fb6733937dbcb57e6cc9f0074fa471aa737950f8
nickmza <nickm40@gmail.com>
2015-10-06 11:49:42 +0200

    Add missing file.

---
+++ Continuum.WebApi/Providers/MailProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace Continuum.WebApi.Providers
+{
+    public class MailProvider : IIdentityMessageService
+    {
+        public System.Threading.Tasks.Task SendAsync(IdentityMessage message)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
