2d301dc623ecb7c434a57095c845ecf0a7e6aa54
nickmza <nickm40@gmail.com>
2015-10-09 14:01:41 +0200

    Update Email Links for Confirmation

--- Continuum.WebApi/Controllers/AccountController.cs
+++ Continuum.WebApi/Controllers/AccountController.cs
@@ -391,14 +391,9 @@ namespace Continuum.WebApi.Controllers
             var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
 
             var newRouteValues = new RouteValueDictionary(new { userId = user.Id, code = code });
-            newRouteValues.Add("httproute", true);
-            System.Web.Mvc.UrlHelper urlHelper = new System.Web.Mvc.UrlHelper(HttpContext.Current.Request.RequestContext, RouteTable.Routes);
-            string callbackUrl = urlHelper.Action(
-                "ConfirmEmail",
-                "Account",
-                newRouteValues,
-                HttpContext.Current.Request.Url.Scheme
-                );
+
+
+            string callbackUrl = "http://continuuumapp.azurewebsites.net/dist/#/confirmation?code=" + code;
 
             string emailTitle = "Please confirm your account";
             string emailBody = "<html><body><p>Confirmation Code: <a href='" + callbackUrl + "'>Please click Here to confirm your email</a></p><body></html>";
