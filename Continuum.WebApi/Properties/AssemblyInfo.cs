b2f652e1f8794d4e1e6b0a5ae0f91d3d9c5bff4c
JohanJStd <johan.jordaan@standardbank.co.za>
2016-02-22 11:42:36 +0200

    Sorted Out Goal Description to allow nulls

--- Continuum.WebApi/Properties/AssemblyInfo.cs
+++ Continuum.WebApi/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Continuum.WebApi")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
